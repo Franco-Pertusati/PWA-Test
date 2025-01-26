7ee8c1c287dbbcdfbf66bfa7408ceb66a9249a8f
Franco Pertusati <pertusatifranco836@gmail.com>
2025-01-26 10:59:54 -0300

    Prueba de notificaciones push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GenericBtnComponent } from './ui/generic-btn/generic-btn.component';
-import { InputWithIconComponent } from './ui/input-with-icon/input-with-icon.component';
-import { NavbarComponent } from './ui/navbar/navbar.component';
 import { SidebarComponent } from './ui/sidebar/sidebar.component';
-import { WideBtnComponent } from './ui/wide-btn/wide-btn.component';
-import { ThemeService } from './services/theme.service';
+import { PushNotificationService } from './services/notification-push.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    SidebarComponent,
-  ],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  pushService = inject(PushNotificationService);
+
+  constructor() {
+    this.pushService.listenToPushNotifications();
+  }
+}
